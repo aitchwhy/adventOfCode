5dab94eff11f2c6c32d7c27b7a08094ef38d21ad
hdd2k <hank.lee.qed@gmail.com>
2021-04-02 20:52:23 -0700

    Sync-up in a while

--- 2018/1207.py
+++ 2018/1207.py
@@ -1,8 +1,9 @@
 
 # Imports
-from collections import namedtuple, deque, defaultdict
+from collections import namedtuple, deque, defaultdict, Counter
 import heapq
 import re
+from itertools import count
 
 # String parsing -> named tuple
 Req = namedtuple('Req', ['before', 'after'])
@@ -15,11 +16,13 @@ with open('./1207.in', 'r') as f:
     reqs = [parse(l.strip()) for l in f.readlines()]
 
 # Build matrix (adj. list)
-m_in = defaultdict(list)
-m_out = defaultdict(list)
+m_in, m_in_2 = defaultdict(list), defaultdict(list)
+m_out, m_out_2 = defaultdict(list),defaultdict(list)
 for r in reqs:
     m_out[r.before].append(r.after)
+    m_out_2[r.before].append(r.after)
     m_in[r.after].append(r.before)
+    m_in_2[r.after].append(r.before)
 
 ###############################
 # Visualization (Graphviz Digraph)
@@ -28,6 +31,7 @@ for r in reqs:
 with open('./1207.dot', 'w') as f:
     f.write("digraph {\n")
     f.write("rankdir=LR;")
+    f.write("graph [ordering=\"out\"];")
     for k,v in m_out.items():
         # Double "{{" or "}}" to ESCAPE in format string
         f.write("{} -> {{{}}};\n".format(k,' '.join(v)))
@@ -71,6 +75,101 @@ print(''.join(steps))
 ########################################
 # Part 2 - parallel working schedule (construct & how long to complete?)
 ########################################
+
+def writeWorking():
+    with open('./temp.txt', 'w') as f:
+        for w in workingSchedule:
+            f.write(' '.join([(x if (x != None) else '.') for x in w]) + '\n')
+
+
+numWorkers = 5
+
+def workTime(s): return 60 + int(ord(s) - ord('A') + 1)
+
+workingSchedule = [[] for _ in range(numWorkers)]
+
+todos = Counter({ch : workTime(ch) for ch in ''.join(steps)})
+print("TODOS : {}".format(todos))
+
+zero_incoming = [n for n in m_out_2.keys() if (len(m_in_2[n]) == 0)]
+heapq.heapify(zero_incoming)
+totalTime = 0
+finished = []
+
+# Updates 'zero_incoming' to hold currently working projects
+# (guaranteed topological sorted order, breaking ties alphabetically)
+def updateWorkable():
+    assert(len(zero_incoming) > 0)
+    # Curr
+    curr = heapq.heappop(zero_incoming)
+    for child in m_out_2[curr].copy():
+        # remove edge
+        m_out_2[curr].remove(child)
+        m_in_2[child].remove(curr)
+        # Add child to zero-incoming if no incoming
+        if (len(m_in_2[child]) == 0):
+            heapq.heappush(zero_incoming, child)
+
+    print("UPDATED zero_incoming : {}".format(zero_incoming))
+
+# Work
+for t in count():
+    # Each prev worked accounted
+    if (t > 0):
+        todos -= Counter([w[-1] for w in workingSchedule if w[-1] != None])
+
+    # Each worker curr work assignments
+    for worker in workingSchedule:
+        prevWork = None if (t == 0) else worker[-1]
+        currWorking = [w[-1] for w in workingSchedule if len(w) > 0]
+
+        # no prev work -> Get from (workable - curr working other workers)
+        # prev work not finished -> continue working
+        # prev work finished -> update workable to new
+
+        # Update workables <--- prevWork & count == 0
+        if ((prevWork != None) and (todos[prevWork] == 0)):
+            finished.append(prevWork)
+            updateWorkable()
+        
+        canWork = [n for n in zero_incoming if ((n not in finished) and (n not in currWorking))]
+
+        # New work or Old work or None
+        if ((prevWork != None) and (todos[prevWork] > 0)):
+            # Unfinished work -> continue
+            worker.append(prevWork)
+        else:
+            # print("@@@@@@@@@@@@@@@@@@@@@")
+            # print("prev Work : {}".format(prevWork))
+            # print("curr working : {}".format(currWorking))
+            # print("zero_incoming : {}".format(zero_incoming))
+            # print("canWork : {}".format(canWork))
+
+            # None or finished work
+            worker.append(sorted(canWork)[0] if (len(canWork) > 0) else None)
+
+            # print("@@@@@@@@@@@@@@@@@@@@@")
+            # print("prev Work : {}".format(prevWork))
+            # print("curr working : {}".format(currWorking))
+            # print("zero_incoming : {}".format(zero_incoming))
+            # print("canWork : {}".format(canWork))
+            # if (t > 100):
+            #     writeWorking()
+            #     exit()
+
+    # Finish condition
+    if (sum(todos.values()) == 0): break
+    # print(len(zero_incoming))
+    # print("sum(TODO) : {}".format(sum(todos.values())))
+    totalTime += 1
+
+
+writeWorking()
+print(finished)
+# 1183 --- too high
+print(totalTime)
+
+
 
 ########################################
 # Lessons
