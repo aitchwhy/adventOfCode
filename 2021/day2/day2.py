6b20bbd37a321e01cdd26216617b9b3244228d41
hdd2k <hank.lee.qed@gmail.com>
2022-08-17 11:52:39 -0700

    Finish 2021 day 2 part 2

--- 2021/day2/day2.py
+++ 2021/day2/day2.py
@@ -7,12 +7,13 @@ from lib2to3.pgen2 import token
 
 
 class SubState:
-    def __init__(self, depth, dist):
+    def __init__(self, depth, dist, aim):
         self.depth = depth
         self.dist = dist
+        self.aim = aim
 
     def __str__(self):
-        return f"{self.depth} {self.dist}"
+        return f"{self.depth} {self.dist} {self.aim}"
 
 
 FORWARD, DOWN, UP = 'forward', 'down', 'up'
@@ -29,15 +30,17 @@ def parseLine(l):
 
 def solve(lineContents):
     # part 1 - calculate state after running all instructions.
-    part1State = SubState(0, 0)
+    part1State = SubState(0, 0, 0)
     for l in lineContents:
         parsed = parseLine(l)
+        amt = parsed[1]
         if parsed[0] == FORWARD:
-            part1State.dist += parsed[1]
+            part1State.dist += amt
+            part1State.depth += (part1State.aim * amt)
         elif parsed[0] == UP:
-            part1State.depth -= parsed[1]
+            part1State.aim -= amt
         elif parsed[0] == DOWN:
-            part1State.depth += parsed[1]
+            part1State.aim += amt
 
     print(part1State, part1State.dist * part1State.depth)
 
