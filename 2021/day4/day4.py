dff5952958e61bfa2e2cbb7cf9e019138908a6a5
hdd2k <hank.lee.qed@gmail.com>
2022-08-24 14:31:56 -0700

    Add 2021 day 4 part 1

--- 2021/day4/day4.py
+++ 2021/day4/day4.py
@@ -1,17 +1,86 @@
 
 # TODO: create class for board
 class Board():
-    def __init__(self, board):
-        pass
+    def __init__(self, boardlines):
+        self.N = len(boardlines)
+        self.boardlines = boardlines
+        self.boardcalled = [[False] * self.N for _ in range(self.N)]
+        self.lastCalled = None
+
+    def addNum(self, num):
+        # check number if exists + update
+        for i in range(self.N):
+            for j in range(self.N):
+                if self.boardlines[i][j] == num:
+                    self.boardcalled[i][j] = True
+
+        # Update last called number
+        self.lastCalled = num
+
+    # returns (isBingo, isRow, bingoIdx)
+    def hasBingo(self):
+        for i in range(self.N):
+            # row bingo
+            if all(self.boardcalled[i]):
+                return (True, True, i)
+            # columns bingo
+            if all([self.boardcalled[j][i] for j in range(self.N)]):
+                return (True, False, i)
+        return (False, None, None)
+
+    def getLastCalled(self):
+        return self.lastCalled
+
+    def getUncalledSum(self):
+        uncalledSum = 0
+        for i in range(self.N):
+            for j in range(self.N):
+                if not self.boardcalled[i][j]:
+                    uncalledSum += self.boardlines[i][j]
+        return uncalledSum
 
     def __repr__(self):
-        return f"this is a board"
+        finalStr = "\n"
+        for i, numList in enumerate(self.boardlines):
+            for j, num in enumerate(numList):
+                finalStr += f"{str(num)}[{self.boardcalled[i][j]}]" + " "
+            finalStr += "\n"
+        return finalStr
 
 
 def solve(inputLines):
     print("hello")
     print(inputLines)
+    linesIter = iter(inputLines)
 
-    # TODO: parse number sequence
+    # parse number sequence
+    numSeqStr = next(linesIter)
+    numSeq = [int(x) for x in numSeqStr.strip().split(",")]
+    print(f"number sequence : {numSeq}")
 
     # TODO: parse boards
+    boards = []
+    while (True):
+        blankLine = next(linesIter, None)
+        if (blankLine is None):
+            break
+        boardLines = []
+        for _ in range(5):
+            boardLine = [int(x) for x in (next(linesIter).strip().split())]
+            boardLines.append(boardLine)
+        boards.append(Board(boardLines))
+    for n in numSeq:
+        for b in boards:
+            b.addNum(n)
+            bingoInfo = b.hasBingo()
+            if bingoInfo[0]:
+                print(f"bingo with winning board : {b}")
+                print(f"winning board info : {bingoInfo}")
+                lastCalled = b.getLastCalled()
+                uncalledSum = b.getUncalledSum()
+                print(f"last called : {lastCalled}")
+                print(f"final number : {lastCalled * uncalledSum}")
+                return
+                # return b.getLastCalled()
+        print(f"boards : {boards}")
+    # print(board)
