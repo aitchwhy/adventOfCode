668523c8dd96205867cd3a8f8fdb4d362192b2a9
hdd2k <hank.lee.qed@gmail.com>
2022-08-25 11:03:30 -0700

    Add 2021 day 5 part 2

--- 2021/day5/day5.py
+++ 2021/day5/day5.py
@@ -3,8 +3,9 @@
 # y : top<>bottom
 class Board():
     def __init__(self, allLines):
-        # consider only straight lines
-        lines = [l for l in allLines if l.isStraight()]
+        # consider only straight lines + diagonal lines
+        lines = [l for l in allLines if (l.isStraight() or l.isDiagonal())]
+        # print(f"--------- lines: {lines}")
 
         xLen, yLen = float('-inf'), float('-inf')
         for l in lines:
@@ -20,13 +21,26 @@ class Board():
         for l in lines:
             x1, y1 = l.start
             x2, y2 = l.end
-            # print(f"x : {x1},{y1}, y: {x2},{y2}")
+            print(f"x : {x1},{y1}, y: {x2},{y2}")
             if x1 == x2:
                 for y in range(min(y1, y2), max(y1, y2) + 1):
                     self.board[y][x1] += 1
             elif y1 == y2:
                 for x in range(min(x1, x2), max(x1, x2) + 1):
                     self.board[y1][x] += 1
+            else:
+                xStep = (1, 0) if ((x2 - x1) > 0) else (-1, 0)
+                yStep = (0, 1) if ((y2 - y1) > 0) else (0, -1)
+                curr = [x1, y1]
+                while (curr != [x2, y2]):
+                    self.board[curr[1]][curr[0]] += 1
+                    curr[0] += xStep[0]
+                    curr[1] += yStep[1]
+                self.board[y2][x2] += 1
+
+            # print("#########")
+            # print(self)
+            # print("#########")
 
     def getSpotCount(self, x, y):
         return self.board[y][x]
@@ -56,6 +70,9 @@ class Line():
     def isStraight(self):
         return ((self.start[0] == self.end[0] and self.start[1] != self.end[1])
                 or (self.start[0] != self.end[0] and self.start[1] == self.end[1]))
+
+    def isDiagonal(self):
+        return abs(self.start[0] - self.end[0]) == abs(self.start[1] - self.end[1])
 
     def __repr__(self) -> str:
         return f"{self.start}->{self.end}"
