2d43cede39be57e054809fccb89ee948db7672ad
hdd2k <hank.lee.qed@gmail.com>
2022-09-01 13:33:20 -0700

    Update main entry to be non-relative + add day7 skeleton

---
+++ 2021/day7/day7.py
@@ -0,0 +1,4 @@
+
+
+def solve(lineContents):
+    print("Day 7 solution")
--- 2021/main.py
+++ 2021/main.py
@@ -8,14 +8,15 @@ import day3.day3 as d3
 import day4.day4 as d4
 import day5.day5 as d5
 import day6.day6 as d6
+import day7.day7 as d7
 
 # import utils
 import utils.utils as utils
 
 # Main directory
-mainDir = pathlib.Path(".")
+# mainDir = pathlib.Path(".")
+mainDir = pathlib.Path(__file__).parent
 
-# asdfasdf
 print("######################")
 print(pathlib.Path("."))
 print(pathlib.Path.cwd())
@@ -30,4 +31,5 @@ if __name__ == "__main__":
     # d3.solve(utils.readFile(mainDir / "day3/input.in"))
     # d4.solve(utils.readFile(mainDir / "day4/input.in"))
     # d5.solve(utils.readFile(mainDir / "day5/input.in"))
-    d6.solve(utils.readFile(mainDir / "day6/input.in"))
+    # d6.solve(utils.readFile(mainDir / "day6/input.in"))
+    d7.solve(utils.readFile(mainDir / "day7/sample.in"))
