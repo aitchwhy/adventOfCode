9fc30a101ea118f66df1b0606199bd92c105dc6d
hdd2k <hank.lee.qed@gmail.com>
2022-10-26 02:46:28 -0700

    Add comments on day8 approach solution

--- 2021/day8/day8.py
+++ 2021/day8/day8.py
@@ -1,6 +1,6 @@
 
 def parseEntry(content):
-    # parse input. (10 unique signal patterns | output values)
+    # parse input. (10 unique signal patterns | 4 output values)
     # print(f"parsing line {signals} and {output}")
     tokens = content.split(" ")
 
@@ -8,9 +8,9 @@ def parseEntry(content):
     from itertools import groupby
     grouped = [list(lst) for k, lst in groupby(
         tokens, key=lambda x: x == "|") if (k is False)]
-    # print(grouped)
 
     entry = Entry(signals=grouped[0], output=grouped[1])
+    print(entry)
     return entry
 
 
@@ -18,9 +18,24 @@ class Entry:
     def __init__(self, signals, output) -> None:
         self.signals = signals
         self.output = output
+        # maps jumbled->output
+        self.jumbleMap: dict[str, str] = dict()
+
+    # orig mapping : a,b,c,d,e,f,g - topleft to bottomright
+    def solve(self):
+        # identify unique numbers in jumbled.
+        # 1,4,7,8 -> unique
+        # 2,3,5,6,9,0 -> not unique
+        # 2 - 5 segs. (of 2,5) - when subtract "4", 3 len left.
+        # 3 - 5 segs. (of 2,3,5). When unioned with "1". no change.
+        # 5 - 5 segs. (of 2,5) - when subtract "4", 2 len left.
+        # 6 - 6 segs. (of 6,9,0) - when subtract "1" - only 1 removed.
+        # 9 - 6 segs. (of 6,9) - when subtract "4" - 3 removed.
+        # 0 - 6 segs. (of 6,9) - when subtract "4" - 4 removed.
+        pass
 
     def __repr__(self) -> str:
-        return f"signals {self.signals} and output {self.output}"
+        return f"signals {self.signals} and output {self.output} with mapping {self.jumbleMap}"
 
 
 def solve(lineContents):
@@ -38,6 +53,8 @@ def solve(lineContents):
             break
 
     # print(entries)
+
+    # part 1. Find in output how many times unique numbers (1,4,7,8) appear
     count = 0
     for entry in entries:
         for o in entry.output:
@@ -45,6 +62,7 @@ def solve(lineContents):
                 count += 1
     print(count)
 
-    # part 1. Find in output
-
-    # part 2.
+    # part 2. Decode all jumbled signals. Then sum all 4 digit numbers.
+    for entry in entries:
+        entry.solve()
+        print(f"solved entry : {entry}")
