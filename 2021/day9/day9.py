db733f11a7075368f67e1c785843d6255638262f
hdd2k <hank.lee.qed@gmail.com>
2022-10-28 08:54:18 -0700

    Add day9

--- 2021/day9/day9.py
+++ 2021/day9/day9.py
@@ -29,6 +29,29 @@ class Map():
         # height plus 1
         return int(self.getHeight(x, y)) + 1
 
+    def findReachable(self, x, y, canMoveFunc):
+        '''
+        Find all reachable points by doing BFS starting from (x,y)
+
+        canMoveFunc: (x1,y1,x2,y2) -> bool. Can tell if can move from (x1,y1) to (x2,y2)
+        '''
+        visited = set()
+
+        from collections import deque
+        toVisit = deque()
+        toVisit.append((x, y))
+
+        while len(toVisit) > 0:
+            curr = toVisit.popleft()
+            # visit curr
+            if not curr in visited:
+                # add neighbors
+                for n in self.getNeighbors(curr[0], curr[1]):
+                    if canMoveFunc(curr[0], curr[1], n[0], n[1]):
+                        toVisit.append(n)
+                visited.add(curr)
+        return visited
+
     def __repr__(self) -> str:
         finalStr = "######\n"
         for l in self.mapMatrix:
@@ -56,11 +79,45 @@ def solve(lineContents):
 
     # part 1. Find all low points and get risk sum of all low points.
     riskSum = 0
+    lowPoints = []
     for x in range(mapMat.xMax):
         for y in range(mapMat.yMax):
             currHeight = mapMat.getHeight(x, y)
             neighbors = mapMat.getNeighbors(x, y)
+            # is low point (lower than all neighbors)
             if all(mapMat.getHeight(*n) > currHeight for n in neighbors):
+                lowPoints.append((x, y))
                 riskSum += mapMat.getRisk(x, y)
 
-    print(riskSum)
+    print(f"riskSum : {riskSum} --- lowPoints : {lowPoints}")
+
+    # part 2. Find all "basins" (points that flow to low points delineated by 9's).
+    # Find largest 3 basins and multiply their areas.
+    # size of basin is num(locations) within basin including low point.
+
+    # Approach - find low points. from each low point, BFS to find all reachable until 9's.
+
+    # TODO: sample -> basin sizes 3,9,14,9 (largest 3 mult = 1134)
+
+    # can move to if NOT "9" and is higher number
+    def canMoveTo(x1, y1, x2, y2):
+        isHigherNum = mapMat.getHeight(x1, y1) < mapMat.getHeight(x2, y2)
+        isNot9 = mapMat.getHeight(x2, y2) != 9
+        return (isHigherNum and isNot9)
+
+    # all basin sizes
+    basinSizes = []
+    # use heap to get max 3 basin sizes (min-heap)
+    import heapq
+    for lowPoint in lowPoints:
+        lowX, lowY = lowPoint
+        reachable = mapMat.findReachable(lowX, lowY, canMoveTo)
+        # print(f"lowPoint : {lowPoint} --- reachable : {(reachable)}")
+        heapq.heappush(basinSizes, -1 * len(reachable))
+
+    print(basinSizes)
+
+    final = 1
+    for i in range(3):
+        final *= (-1 * heapq.heappop(basinSizes))
+    print(final)
