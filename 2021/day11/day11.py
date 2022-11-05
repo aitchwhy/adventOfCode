08ea1556276b9a5e28fed9fcfcb207a5129b9ab7
hdd2k <hank.lee.qed@gmail.com>
2022-11-04 21:24:14 -0700

    Add day 11 part 1 base grid class

--- 2021/day11/day11.py
+++ 2021/day11/day11.py
@@ -1,8 +1,60 @@
 
+
+# TODO: custom Grid "+" operator to add 2 grids together.
+# https://www.codingem.com/python-__add__-method/
+
+class Grid():
+    # 2D grid.
+    # For debug convenience, "x" is left<>right index (leftside 0)
+    # For debug convenience, "y" is top<>down index (topside 0)
+    def __init__(self, xLen, yLen) -> None:
+        self.grid = [[0]*(xLen) for _ in range(yLen)]
+
+    def __repr__(self) -> str:
+        # TODO: 2d grid repr string
+        finalStr = "######\n"
+        for l in self.grid:
+            finalStr += ("".join([str(x) for x in l]) + "\n")
+        finalStr += "######"
+        return finalStr
+
+    def get(self, x, y) -> int:
+        pass
+
+    def set(self, x, y):
+        pass
+
+
+class EnergyState(Grid):
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+
+class EnergyDelta(Grid):
+    def __init__(self) -> None:
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def isEmpty(self) -> bool:
+        '''
+        Check if ALL energy is 0 -> return True
+        '''
+        return True
+
+
 def solve(lineContents):
     print(f"day 11 lineContents : {lineContents}")
 
     # TODO: Parse input (10x10) grid where each cell is octopus energy level.
+    x = Grid(8, 12)
+    x.grid[1][2] = 1
+    print(x)
+
     # Each step,
     # - EACH octopus energy +1.
     # - if energy > 9, "flash" occurs (all 8 adjacent +1 energy) and
