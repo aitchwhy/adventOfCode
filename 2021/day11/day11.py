5dc891dd72620d81e3ffa2b8ffbff2706b17e160
hdd2k <hank.lee.qed@gmail.com>
2022-11-04 21:12:55 -0700

    Add day 11 explanation and methodology pseudo code

--- 2021/day11/day11.py
+++ 2021/day11/day11.py
@@ -1,3 +1,19 @@
 
 def solve(lineContents):
     print(f"day 11 lineContents : {lineContents}")
+
+    # TODO: Parse input (10x10) grid where each cell is octopus energy level.
+    # Each step,
+    # - EACH octopus energy +1.
+    # - if energy > 9, "flash" occurs (all 8 adjacent +1 energy) and
+    # - "flash" loop until no more
+    # - When no more "flash" -> those who "flashed" energy = 0.
+
+    # Note: feels like setting to 0 at end is deliberate wording. But NVM for now.
+    # Can set to 0 right after "flash" occurs.
+
+    # part 1. given initial state, how many TOTAL flashes after 100 steps?
+    # TODO: 2 10x10 grids.
+    # - "energyStates" : One for current energy state
+    # - "energyDeltas" : one for 1 iteration of energy delta to apply (result of "flash")
+    #   - Use energyDeltas to update E state each loop - do while all delta != 0
