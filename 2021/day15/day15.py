b9da13455e9516bda459ff5bc30a0b61d1f5e34f
hdd2k <hank.lee.qed@gmail.com>
2022-11-14 17:48:31 -0800

    Add day 15 part 1 naive DFS approach

--- 2021/day15/day15.py
+++ 2021/day15/day15.py
@@ -5,6 +5,7 @@
 # DP -> for each cell, compute smallest from curr-end. Re-use prev calculated (e.g. min dist from cells closer to end)
 
 class Graph():
+
     # X : left<>right (left = 0)
     # Y : top<>bottom (top = 0)
     def __init__(self, lines) -> None:
@@ -14,13 +15,73 @@ class Graph():
         for xIdx in range(self.xDim):
             for yIdx in range(self.yDim):
                 self.g[yIdx][xIdx] = int(lines[yIdx][xIdx])
+        self.START = (0, 0)
+        self.END = (self.yDim-1, self.xDim-1)
 
     def __repr__(self) -> str:
         finalStr = "##########\n"
+        finalStr += f"xDim : {self.xDim} --- yDim: {self.yDim}\n"
+        finalStr += "##########\n"
         for row in self.g:
             finalStr += ("|".join(str(x) for x in row) + "\n")
         finalStr += "##########"
         return finalStr
+
+    # Total paths. (()! / ()!()!)
+
+    # Naive solution - DFS.
+    def dfs(self):
+        # current path tracked til BEFORE current.
+        path = []
+        finalPathsWithRiskSum = []
+        minRisk, minRiskPath = float("inf"), None
+
+        # Since no NEGATIVE numbers, if revisit a node, then more costly than just 1 pass.
+        # if visited, do not visit.
+        def traverse(currNode):
+            nonlocal minRisk, minRiskPath, path, finalPathsWithRiskSum
+            assert currNode is not None, f"should NOT visit NULL currNode"
+
+            # visit curr (do not visit if visited)
+            if (currNode in path):
+                return
+
+            path.append(currNode)
+
+            # Reached end
+            if (currNode == self.END):
+                # risk does NOT count start.
+                currPathRiskSum = sum([self.g[n[1]][n[0]] for n in path[1:]])
+
+                if (currPathRiskSum < minRisk):
+                    minRisk = currPathRiskSum
+                    minRiskPath = path.copy()
+                finalPathsWithRiskSum.append((path.copy(), currPathRiskSum))
+                # unvisit curr
+                path.pop()
+                return
+
+            # neighbors
+            offsets = ((0, 1), (1, 0))  # bottom, right ONLY
+            for o in offsets:
+                nextNode = (currNode[0] + o[0], currNode[1] + o[1])
+                isInBoundary = (
+                    (0 <= nextNode[0] < self.xDim) and (0 <= nextNode[1] < self.yDim))
+                if isInBoundary:
+                    # visit next
+                    traverse(nextNode)
+
+            # unvisit curr (cleanup)
+            path.pop()
+
+        # Start at top-left
+        traverse(self.START)
+
+        return minRisk, minRiskPath
+
+    # DP (memoized solution)
+    def dp(self):
+        pass
 
 
 def solve(lines):
@@ -34,5 +95,7 @@ def solve(lines):
     print(lines)
     graph = Graph(lines)
     print(graph)
+    minRisk, minRiskPath = graph.dfs()
+    print(f"minRisk: {minRisk} --- minRiskPath: {minRiskPath}")
 
     # part 2.
