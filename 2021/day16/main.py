fc57d865e4f5eaf5c935ba2db6096adb031a489c
hdd2k <hank.lee.qed@gmail.com>
2023-03-20 14:47:58 -0700

    Saving old state

---
+++ 2021/day16/main.py
@@ -0,0 +1,4 @@
+
+
+def solve(lines):
+    print("Solving day 16")
--- 2021/main.py
+++ 2021/main.py
@@ -17,8 +17,7 @@ import day12.day12 as d12
 import day13.day13 as d13
 import day14.day14 as d14
 import day15.day15 as d15
-import day15.copiedDay15 as copiedd15
-# import day16.main as d16
+import day16.main as d16
 # import day17.main as d17
 # import day18.main as d18
 # import day19.main as d19
@@ -59,9 +58,6 @@ if __name__ == "__main__":
     # d12.solve(utils.readFile(mainDir / "day12/input.in"))
     # d13.solve(utils.readFile(mainDir / "day13/input.in"))
     # d14.solve(utils.readFile(mainDir / "day14/input.in"))
-
-    # copiedd15.solve(utils.readFile(mainDir / "day15/sample.in"))
-    copiedd15.solve(utils.readFile(mainDir / "day15/input.in"))
-
     # d15.solve(utils.readFile(mainDir / "day15/sample.in"))
-    # d15.solve(utils.readFile(mainDir / "day15/input.in"))
+    d16.solve(utils.readFile(mainDir / "day16/input.in"))
+    # d16.solve(utils.readFile(mainDir / "day16/sample.in"))
