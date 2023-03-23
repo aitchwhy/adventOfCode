e1df6a198d27c949ca03a920953ada28e075cf66
hdd2k <hank.lee.qed@gmail.com>
2023-03-23 13:46:30 -0700

    Update to use newer asyncio.run syntax

--- playground/asyncio_async.py
+++ playground/asyncio_async.py
@@ -19,10 +19,11 @@ async def download_all(urls):
         # gather results
         await asyncio.gather(*tasks, return_exceptions=True)
 
+# Downloaded 160 in 0.7436487674713135 seconds
 
 if __name__ == "__main__":
     st = time.time()
-    asyncio.get_event_loop().run_until_complete(download_all(sites))
+    asyncio.run(download_all(sites))
     et = time.time()
     duration = (et - st)
     print(f"Downloaded {len(sites)} in {duration} seconds")
