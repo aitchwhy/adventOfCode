15b7b16830e7ec795ea97dd03d81116cbe82e336
hdd2k <hank.lee.qed@gmail.com>
2023-03-23 21:23:06 -0700

    Update async py file to work queue generator yield

--- playground/async_tutorial/main.py
+++ playground/async_tutorial/main.py
@@ -1,21 +1,21 @@
 import queue
 
 
-def task(name, work_queue):
-    if work_queue.empty():
-        print(f"Task {name} nothing to do")
-    else:
-        while not work_queue.empty():
-            count = work_queue.get()
-            total = 0
-            print(f"Task {name} running")
-            for x in range(count):
-                total += 1
-            print(f"Task {name} total: {total}")
+def task(name, q):
+    while not q.empty():
+        count = q.get()
+        total = 0
+        print(f"Task {name} running")
+        for x in range(count):
+            total += 1
+            yield
+        print(f"Task {name} total: {total}")
 
 
 def main():
     # main entry
+
+    # Create Q of work
     work_queue = queue.Queue()
 
     # put work in Q
@@ -24,13 +24,20 @@ def main():
 
     # create workers to process Q
     tasks = [
-        (task, "1", work_queue),
-        (task, "2", work_queue),
+        task("1", work_queue),
+        task("2", work_queue),
     ]
 
     # run tasks
-    for t, n, q in tasks:
-        t(n, q)
+    done = False
+    while not done:
+        for t in tasks:
+            try:
+                next(t)
+            except StopIteration:
+                tasks.remove(t)
+            if len(tasks) == 0:
+                done = True
 
 
 if __name__ == "__main__":
