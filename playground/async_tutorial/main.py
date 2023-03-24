6a9a7e814282ae3a41c4eb3f66a38e8ba9c0f2ff
hdd2k <hank.lee.qed@gmail.com>
2023-03-23 21:09:52 -0700

    Add async with starter sync version

---
+++ playground/async_tutorial/main.py
@@ -0,0 +1,37 @@
+import queue
+
+
+def task(name, work_queue):
+    if work_queue.empty():
+        print(f"Task {name} nothing to do")
+    else:
+        while not work_queue.empty():
+            count = work_queue.get()
+            total = 0
+            print(f"Task {name} running")
+            for x in range(count):
+                total += 1
+            print(f"Task {name} total: {total}")
+
+
+def main():
+    # main entry
+    work_queue = queue.Queue()
+
+    # put work in Q
+    for work in [15, 10, 5, 2]:
+        work_queue.put(work)
+
+    # create workers to process Q
+    tasks = [
+        (task, "1", work_queue),
+        (task, "2", work_queue),
+    ]
+
+    # run tasks
+    for t, n, q in tasks:
+        t(n, q)
+
+
+if __name__ == "__main__":
+    main()
